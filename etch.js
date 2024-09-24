6ffa1af0b7d50e400d64e03e048a38851312b7d7
Erick CB <erickdani10@hotmail.com>
2024-09-24 15:10:14 -0600

    Add logic to grid density selector

--- etch.js
+++ etch.js
@@ -1,8 +1,6 @@
 const container = document.querySelector(".container");
 const gridInput = document.querySelector("#dimensions");
-const gridButton = document.querySelector("#createBtn");
-
-
+const gridButton = document.querySelector(".createBtn");
 
 function createGridElements(dimension) {
   for (let i = 1; i <= dimension; i++) {
@@ -18,8 +16,26 @@ function createGridElements(dimension) {
 
 createGridElements(gridInput.value);
 
-container.addEventListener("mouseover", (event) => {
-  let target = event.target;
-  target.classList.add("black");
-});
+function pixelListener (){
+	container.addEventListener("mouseover", (event) => {
+		let target = event.target;
+		target.classList.add("black");
+	});
+}
 
+pixelListener();
+
+function eliminateGrid(){
+	while(container.hasChildNodes()){
+		container.removeChild(container.lastChild);
+	}
+}
+
+gridButton.addEventListener("click", () => {
+	if(gridInput.value < 50){
+		eliminateGrid();
+		createGridElements(gridInput.value);
+	} else {
+		alert("Max grid density: 50x50")
+	}
+})
