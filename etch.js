1992e7ef5292825c7935019de88efaf82e6e3d31
Erick CB <erickdani10@hotmail.com>
2024-09-24 13:31:00 -0600

    Add dynamically-created grid

---
+++ etch.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+
+function createGridElements(dimension) {
+  for (let i = 1; i <= dimension; i++) {
+    for (let j = 1; j <= dimension; j++) {
+      const pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      container.appendChild(pixel);
+    }
+  }
+}
