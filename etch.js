f97140845854782ec9165ad5571e7dfe9a0812ef
Erick CB <erickdani10@hotmail.com>
2024-09-24 14:43:50 -0600

    Adjust grid-creation logic for dinamic pixel density

--- etch.js
+++ etch.js
@@ -1,18 +1,25 @@
 const container = document.querySelector(".container");
+const gridInput = document.querySelector("#dimensions");
+const gridButton = document.querySelector("#createBtn");
+
+
 
 function createGridElements(dimension) {
   for (let i = 1; i <= dimension; i++) {
     for (let j = 1; j <= dimension; j++) {
       const pixel = document.createElement("div");
       pixel.classList.add("pixel");
+			pixel.style.setProperty('flex-basis', `${800/dimension}px`);
+			pixel.style.setProperty('height', `${800/dimension}px`);
       container.appendChild(pixel);
     }
   }
 }
 
-createGridElements(16);
+createGridElements(gridInput.value);
 
 container.addEventListener("mouseover", (event) => {
   let target = event.target;
   target.classList.add("black");
 });
+
